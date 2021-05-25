73cb2ca2e5882883d0ade4bb5ca61f4f21384e24
Amjad <a.alwareh25@gmail.com>
2021-05-25 15:59:43 +0300

    Set themes colors.xml Created SplashActivity.kt

--- app/src/main/java/com/safeboda/test/MainActivity.kt
+++ app/src/main/java/com/safeboda/test/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.safeboda.test
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+fun Context.newMainActivityIntent() = Intent(this, MainActivity::class.java)
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/safeboda/test/splash/presentation/ui/SplashActivity.kt
@@ -0,0 +1,34 @@
+package com.safeboda.test.splash.presentation.ui
+
+import android.os.Bundle
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.safeboda.test.databinding.ActivitySplashBinding
+import com.safeboda.test.newMainActivityIntent
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.imageView.startAnimation(
+            AnimationUtils.loadAnimation(this, android.R.anim.fade_in)
+        )
+
+        lifecycleScope.launch {
+            delay(2048)
+            startActivity(newMainActivityIntent())
+            finish()
+        }
+    }
+}
